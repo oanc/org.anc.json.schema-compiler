57763bc38307f34d584376390daab539149bc168
Keith Suderman <suderman@anc.org>
2015-06-11 21:48:30 -0400

    Updated dependency versions.

--- src/main/java/org/anc/json/compiler/Version.java
+++ src/main/java/org/anc/json/compiler/Version.java
@@ -6,7 +6,7 @@ package org.anc.json.compiler;
  */
 public final class Version
 {
-	private static final String version = "1.1.0";
+	private static final String version = "1.2.0-SNAPSHOT";
 
 	public static String getVersion() { return version; }
 }
